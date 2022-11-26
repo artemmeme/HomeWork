1e21e9fa95b8bd3a172c3b4bb37fd96cc9ac717c
Артем Шустов <artemmeme2017@yandex.ru>
2022-11-26 17:16:52 +0300

    Написал программу для нахождения 3 числа в числе любой размерности

--- Dz_2/Program.cs
+++ Dz_2/Program.cs
@@ -1,4 +1,16 @@
 ﻿Console.WriteLine("Введите число");
 int num1 = Convert.ToInt32(Console.ReadLine());
-int num2 = (num1/10)%10;
-Console.WriteLine($"Второе число - {num2}");
+int num2 = 1;
+if (num1<100)
+{
+   Console.WriteLine("Третьего числа нет"); 
+}
+else
+{
+    while (num1>1000)
+    {
+    num1 = num1/10;
+    num2 = num1 % 10;
+    }
+    Console.WriteLine($"Третье число - {num2}");
+}
