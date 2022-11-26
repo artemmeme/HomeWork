eb2c8a4fe02276891e4b69eb4a418ea9091faf8e
Артем Шустов <artemmeme2017@yandex.ru>
2022-11-26 16:57:08 +0300

    Программа для выведения Будний/Выходной день

--- Dz_3/Program.cs
+++ Dz_3/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите день недели цифрой от 1 до 7");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num==6)
+{
+    Console.WriteLine("Этот день - выходной");
+}
+else if (num==7)
+{
+        Console.WriteLine("Этот день - выходной");
+}
+else
+{
+   Console.WriteLine("Этот день - будний"); 
+}
